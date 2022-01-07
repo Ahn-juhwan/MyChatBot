b0cebd02aaaa2cd666d1d0a1e2b30ed08c8dad1d
MS <2206017@naver.com>
2022-01-07 19:27:49 +0900

    init: spring boot init

---
+++ src/main/java/com/ja/mychatbot/MychatbotApplication.java
@@ -0,0 +1,13 @@
+package com.ja.mychatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MychatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MychatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ja/mychatbot/MychatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ja.mychatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MychatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
